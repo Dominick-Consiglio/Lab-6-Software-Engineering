5de3bd4ec93aeeb375c6cf8f919fbfcd843808e0
John <jcooper6@ufl.edu>
2024-03-17 14:31:44 -0400

    Added the decode() function

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def encode(password):
     return encoded_password
 
 
+def decode(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_digit = (int(digit) - 3) % 10
+        decoded_password += str(decoded_digit)
+    return decoded_password
+
+
 def main():
     while True:
         print("Menu\n"
@@ -21,7 +29,7 @@ def main():
             encoded_password = encode(password)
             print("Your password has been encoded and stored!\n")
         elif option == "2":
-            decoded_password = decode()
+            decoded_password = decode(encoded_password)
             print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.\n")
         elif option == "3":
             break
